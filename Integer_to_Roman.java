dec9cdb893a5c22399cdb52508283ff26404b013
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2022-12-16 16:20:35 +0530

    Create Integer_to_Roman.java

---
+++ Integer_to_Roman.java
@@ -0,0 +1,17 @@
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+class Solution {
+    public String intToRoman(int num) {
+        int value[] ={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String roman[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder s = new StringBuilder();
+        int i;
+            for( i=0;i<value.length;i++){
+            while(num>=value[i]){
+                num=num-value[i];
+                s.append(roman[i]);
+            }
+        }
+        return s.toString();
+    }
+}
