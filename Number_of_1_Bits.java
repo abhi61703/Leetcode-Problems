4107152985dd8d5f017b92934378156d60c83cf2
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-01-03 21:35:17 +0530

    Create Number_of_1_Bits.java

---
+++ Number_of_1_Bits.java
@@ -0,0 +1,13 @@
+// Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int cnt=0;
+        while(n!=0){
+            n=n&(n-1);
+            cnt++;
+        }
+        return cnt; 
+    }
+}
