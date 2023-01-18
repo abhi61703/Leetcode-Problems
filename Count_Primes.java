991995edad7596933b942bf465674314f5399f7b
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-01-18 20:20:15 +0530

    Create Count_Primes.java

---
+++ Count_Primes.java
@@ -0,0 +1,19 @@
+// Given an integer n, return the number of prime numbers that are strictly less than n.
+
+ 
+
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] isComposite = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!isComposite[i]) {
+                count++;
+                for (int j = 2; i * j < n; j++) {
+                    isComposite[i * j] = true;
+                }
+            }
+        }
+        return count;
+    }
+}
