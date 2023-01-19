225cf3580278998f0b6d3b0ba90c9e5b8211bc1d
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-01-19 22:26:57 +0530

    Create Subarray_Sum_Divisible_by_K.java

---
+++ Subarray_Sum_Divisible_by_K.java
@@ -0,0 +1,15 @@
+// Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k. A subarray is a contiguous part of an array
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int res = 0;
+        int[] remainders = new int[k];
+        remainders[0] = 1;
+        int prefixSum = 0;
+        for (int num : nums) {
+            prefixSum += num;
+            int remainder = ((prefixSum % k) + k) % k;
+            res += remainders[remainder]++;
+        }
+        return res;
+    }
+}
