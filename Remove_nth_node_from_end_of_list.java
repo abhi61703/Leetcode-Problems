2aad8217f97a66a7e9d0098ae8445ebcbff1de69
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2023-07-04 21:51:43 +0530

    Update Remove_nth_node_from_end_of_list.java

--- Remove_nth_node_from_end_of_list.java
+++ Remove_nth_node_from_end_of_list.java
@@ -7,6 +7,7 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
+ 
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
