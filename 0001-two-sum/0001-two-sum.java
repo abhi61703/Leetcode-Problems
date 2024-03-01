fd5afe210d9a7e4404c2ca7087ae414e74351476
Abhishek R Bhat <100010498+abhi61703@users.noreply.github.com>
2024-03-01 21:43:50 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
-        int temp[] = new int[3];
+        int temp[] = new int[2];
         int k=0;
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
